e1e71ce2fd824d636e03d24adc2061e441044da3
s3chkin <seckins191@gmail.com>
2022-02-11 21:18:23 +0200

    deleted paragraphs

--- MathGame/math.js
+++ MathGame/math.js
@@ -5,7 +5,7 @@ document.getElementById("num2").innerHTML = parseInt(num2);
 var result = num1 + num2;
 
 
-document.getElementById("demo5").innerHTML = result;
+// document.getElementById("demo5").innerHTML = result;
 
 let guess = document.getElementById('txtArea').value;
 guess = Number(guess);
@@ -13,24 +13,25 @@ document.getElementById("demo4").innerHTML = guess;
 
 
 function Check() {
-    // if (guess === result) {
-    //     document.getElementById("num3").innerHTML = "true";
-    // } else if (guess != result) {
-    //     document.getElementById("num3").innerHTML = "false";
-
-    // }
-
-
     let guess = document.getElementById("txtArea").value;
     guess = Number(guess);
 
     if (guess === result) {
-        // alert('Correct');
-        window.location.reload();
-    } else {
-        alert('Sorry. Incorrect. The correct answer was ' + result + '.')
-        window.location.reload();
+        document.getElementById("txtArea").style.border = "thick solid lightgreen";
+        var millisecondsToWait = 1000;
+        setTimeout(function() {
+            window.location.reload();
 
+        }, millisecondsToWait);
+    } else {
+        document.getElementById("txtArea").style.border = "thick solid red";
+
+        setTimeout(function() {
+            // document.getElementById("txtArea").innerHTML = num1 + num2;
+            window.location.reload();
+            // alert('Sorry. Incorrect. The correct answer was ' + result + '.')
+
+        }, 600);
     }
 
 }
