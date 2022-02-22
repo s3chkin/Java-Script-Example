b88c5e0a822aa83fbdbcc545595a42ccd80c44ea
s3chkin <seckins191@gmail.com>
2022-02-22 20:58:36 +0200

    finished project

--- calculation of annual assessment/app.js
+++ calculation of annual assessment/app.js
@@ -32,11 +32,26 @@ function yearCountEvaluation() {
 
 }
 
-function lesson() {
-    let lesson2 = prompt("Име на предмет: ");
+function totalEvaluation() {
+    let sum = 0;
+    let countEvaluation = prompt("Брой оценки: "); //2
+    for (var i = 0; i < countEvaluation; i++) {
+        let evaluations = prompt("Напишете си оценките! ");
+        sum += parseFloat(evaluations);
+    }
+    let reslt = (parseFloat(sum) / countEvaluation).toFixed(2);
+    if (reslt >= 5.50) {
+        document.getElementById("result1").innerHTML = `Отличен ${reslt}`;
+    } else if (reslt >= 4.50) {
+        document.getElementById("result1").innerHTML = `Много добър ${reslt}`;
+    } else if (reslt >= 3.50) {
+        document.getElementById("result1").innerHTML = `Добър ${reslt}`;
+    } else if (reslt == 3) {
+        document.getElementById("result1").innerHTML = `Среден ${reslt}`;
+    } else if (reslt < 3) {
+        document.getElementById("result1").innerHTML = `Слаб ${reslt}`;
+    } else {
+        document.getElementById("result1").innerHTML = `Грешка!`;
+    }
 
-    // =========Тука трябва да има логика за пресмятане на оценка
-
-
-    document.getElementById("result1").innerHTML = `Предмет: ${lesson2} - Оценка(${evaluation})`;
 }
