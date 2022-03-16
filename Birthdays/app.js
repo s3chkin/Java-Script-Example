a965473f7ac62a49fe84b61e680c7af6df02b52a
s3chkin <seckins191@gmail.com>
2022-03-16 21:14:43 +0200

    added on click function

--- Birthdays/app.js
+++ Birthdays/app.js
@@ -1,5 +1,8 @@
 var a = document.getElementsByTagName("value");
-document.getElementById("result").innerHTML = a;
+// document.getElementById("result").innerHTML = a;
+function calculation() {
+    document.getElementById("time").style.display = "block";
+}
 const d = new Date();
 thisDate = d.getDate();
 thisMonth = d.getMonth();
@@ -7,3 +10,7 @@ thisYear = d.getFullYear();
 document.getElementById("demo1").innerHTML = thisDate + ".";
 document.getElementById("demo2").innerHTML = thisMonth + 1 + ".";
 document.getElementById("demo3").innerHTML = thisYear;
+
+document.getElementById("result").innerHTML = `През тази ${thisYear} година ще станете на: ${thisYear - 2004} години.`;
+// document.getElementById("result").innerHTML = 12 - thisMonth;
+// document.getElementById("result").innerHTML = 2004 - thisYear;
