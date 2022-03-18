db9090a66e9acca5faf5c1dcd8003918578acc7d
s3chkin <seckins191@gmail.com>
2022-03-18 20:39:57 +0200

    finished example

--- Birthdays/app.js
+++ Birthdays/app.js
@@ -1,10 +1,5 @@
 var a = document.getElementsByTagName("value");
 
-// let inputYear = document.getElementById('years').value;
-// inputYear = Number(inputYear);
-
-
-// document.getElementById("result").innerHTML = a;
 function calculation() {
     document.getElementById("time").style.display = "block";
 }
@@ -16,17 +11,14 @@ document.getElementById("demo1").innerHTML = thisDate + ".";
 document.getElementById("demo2").innerHTML = thisMonth + 1 + ".";
 document.getElementById("demo3").innerHTML = thisYear;
 
-// document.getElementById("result").innerHTML = `През тази ${thisYear} година ще станете на: ${thisYear - 2004} години.`;
-// document.getElementById("result").innerHTML = 12 - thisMonth;
-// document.getElementById("result").innerHTML = 2004 - thisYear;
 
+function result() {
+    let inputDays = Number(document.getElementById("days").value);
+    let inputMonths = Number(document.getElementById("months").value);
+    let inputYear = Number(document.getElementById("years").value);
 
-window.onload = substract();
-
-function calculate() {
-    let inputYear = Number(document.getElementById('firstNumber').value);
-
-    // document.getElementById("result").innerHTML = `През тази ${thisYear} година ще станете на: ${thisYear - inputYear} години.`;
-    document.getElementById("result").innerHTML = `През тази ${inputYear}`;
+    document.getElementById("result").innerHTML = `През тази ${thisYear} година ще станете на: ${thisYear - inputYear} години.`;
+    // document.getElementById("result").innerHTML = `През тази `;
+    // document.getElementById("result").innerHTML = `През тази ${inputYear}`;
 
 }
