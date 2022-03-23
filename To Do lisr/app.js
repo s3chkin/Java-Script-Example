25bf183f315a593ee087a34f0d31c7f357c8f516
s3chkin <seckins191@gmail.com>
2022-03-23 20:25:04 +0200

    added new project

---
+++ To Do lisr/app.js
@@ -0,0 +1,178 @@
+function add() {
+    var inputValue = document.getElementById("txtInput").value;
+    document.getElementById("product1").innerHTML = inputValue;
+
+    document.getElementById("txtInput").style.display = "none";
+    document.getElementById("btn1").style.display = "none";
+
+
+    document.getElementById("txtInput2").style.display = "block";
+    document.getElementById("btn2").style.display = "block";
+}
+
+function add2() {
+    var inputValue = document.getElementById("txtInput2").value;
+    document.getElementById("product2").innerHTML = inputValue;
+
+    document.getElementById("txtInput2").style.display = "none";
+    document.getElementById("btn2").style.display = "none";
+
+
+    document.getElementById("txtInput3").style.display = "block";
+    document.getElementById("btn3").style.display = "block";
+
+}
+
+function add3() {
+    var inputValue = document.getElementById("txtInput3").value;
+    document.getElementById("product3").innerHTML = inputValue;
+
+    document.getElementById("txtInput3").style.display = "none";
+    document.getElementById("btn3").style.display = "none";
+
+
+    document.getElementById("txtInput4").style.display = "block";
+    document.getElementById("btn4").style.display = "block";
+
+}
+
+function add4() {
+    var inputValue = document.getElementById("txtInput4").value;
+    document.getElementById("product4").innerHTML = inputValue;
+
+    document.getElementById("txtInput4").style.display = "none";
+    document.getElementById("btn4").style.display = "none";
+
+
+    document.getElementById("txtInput5").style.display = "block";
+    document.getElementById("btn5").style.display = "block";
+}
+
+function add5() {
+    var inputValue = document.getElementById("txtInput5").value;
+    document.getElementById("product5").innerHTML = inputValue;
+
+    document.getElementById("txtInput5").style.display = "none";
+    document.getElementById("btn5").style.display = "none";
+
+
+    document.getElementById("txtInput6").style.display = "block";
+    document.getElementById("btn6").style.display = "block";
+}
+
+function add6() {
+    var inputValue = document.getElementById("txtInput6").value;
+    document.getElementById("product6").innerHTML = inputValue;
+
+    document.getElementById("txtInput6").style.display = "none";
+    document.getElementById("btn6").style.display = "none";
+
+
+    document.getElementById("txtInput7").style.display = "block";
+    document.getElementById("btn7").style.display = "block";
+}
+
+function add7() {
+    var inputValue = document.getElementById("txtInput7").value;
+    document.getElementById("product7").innerHTML = inputValue;
+
+    document.getElementById("txtInput7").style.display = "none";
+    document.getElementById("btn7").style.display = "none";
+
+
+    document.getElementById("txtInput8").style.display = "block";
+    document.getElementById("btn8").style.display = "block";
+}
+
+function add8() {
+    var inputValue = document.getElementById("txtInput8").value;
+    document.getElementById("product8").innerHTML = inputValue;
+
+    document.getElementById("txtInput8").style.display = "none";
+    document.getElementById("btn8").style.display = "none";
+
+
+    document.getElementById("txtInput9").style.display = "block";
+    document.getElementById("btn9").style.display = "block";
+}
+
+function add9() {
+    var inputValue = document.getElementById("txtInput9").value;
+    document.getElementById("product9").innerHTML = inputValue;
+
+    document.getElementById("txtInput9").style.display = "none";
+    document.getElementById("btn9").style.display = "none";
+
+
+    document.getElementById("txtInput10").style.display = "block";
+    document.getElementById("btn10").style.display = "block";
+}
+
+function product1() {
+    document.getElementById("product1").style.textDecoration = "line-through";
+    document.getElementById("product1").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product1").style.color = "#b5c1c1";
+}
+
+function product2() {
+    document.getElementById("product2").style.textDecoration = "line-through";
+    document.getElementById("product2").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product2").style.color = "#b5c1c1";
+}
+
+function product3() {
+    document.getElementById("product3").style.textDecoration = "line-through";
+    document.getElementById("product3").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product3").style.color = "#b5c1c1";
+}
+
+function product4() {
+    document.getElementById("product4").style.textDecoration = "line-through";
+    document.getElementById("product4").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product4").style.color = "#b5c1c1";
+}
+
+function product5() {
+    document.getElementById("product5").style.textDecoration = "line-through";
+    document.getElementById("product5").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product5").style.color = "#b5c1c1";
+
+}
+
+function product6() {
+    document.getElementById("product6").style.textDecoration = "line-through";
+    document.getElementById("product6").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product6").style.color = "#b5c1c1";
+}
+
+function product7() {
+    document.getElementById("product7").style.textDecoration = "line-through";
+    document.getElementById("product7").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product7").style.color = "#b5c1c1";
+}
+
+function product8() {
+    document.getElementById("product8").style.textDecoration = "line-through";
+    document.getElementById("product8").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product8").style.color = "#b5c1c1";
+}
+
+function product9() {
+    document.getElementById("product9").style.textDecoration = "line-through";
+    document.getElementById("product9").style.backgroundColor = "#5c9fa16e";
+    document.getElementById("product9").style.color = "#b5c1c1";
+}
+
+function Remove() {
+    // document.getElementById("product1").innerHTML = null;
+    // document.getElementById("product2").innerHTML = null;
+    // document.getElementById("product3").innerHTML = null;
+    // document.getElementById("product4").innerHTML = null;
+    // document.getElementById("product5").innerHTML = null;
+    // document.getElementById("product6").innerHTML = null;
+    // document.getElementById("product7").innerHTML = null;
+    // document.getElementById("product8").innerHTML = null;
+    // document.getElementById("product9").innerHTML = null;
+    window.location.reload();
+
+}
