2467d8cd1092d79a28bc67453b466b699eff5315
s3chkin <seckins191@gmail.com>
2022-03-25 22:17:43 +0200

    added new images and finished sections

--- Restaurant menu/app.js
+++ Restaurant menu/app.js
@@ -6,3 +6,9 @@ function Lunch() {
     document.getElementById("BreakfastsWindow").style.display = "none";
     document.getElementById("container2Lunch").style.display = "block";
 }
+
+function Dinner() {
+    document.getElementById("DinnerSection").style.display = "block";
+    document.getElementById("ShakesSection").style.display = "block";
+
+}
