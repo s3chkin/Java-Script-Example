6ad26e2a7d6209603f4160ef35757df6b72337d6
s3chkin <seckins191@gmail.com>
2022-03-25 21:13:23 +0200

    finished with breakfast section

---
+++ Restaurant menu/app.js
@@ -0,0 +1,8 @@
+// function Breakfast() {
+//     document.getElementById("BreakfastsWindow").style.display = "none";
+// }
+
+function Lunch() {
+    document.getElementById("BreakfastsWindow").style.display = "none";
+    document.getElementById("container2Lunch").style.display = "block";
+}
