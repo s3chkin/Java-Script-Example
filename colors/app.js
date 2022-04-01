1a11ff8e4c41897ec61d2bf89ddcae9d40ad953f
s3chkin <seckins191@gmail.com>
2022-04-01 21:37:49 +0300

    added new example

---
+++ colors/app.js
@@ -0,0 +1,32 @@
+function changeColor1() {
+    document.getElementById("body").style.backgroundColor = "red";
+}
+
+function changeColor2() {
+    document.getElementById("body").style.backgroundColor = "green";
+}
+
+
+function changeColor3() {
+    document.getElementById("body").style.backgroundColor = "blue";
+}
+
+function changeColor4() {
+    document.getElementById("body").style.backgroundColor = "yellow";
+}
+
+function changeColor5() {
+    document.body.style.backgroundColor = "rgb(162, 0, 255)";
+}
+
+function changeColor6() {
+    document.body.style.backgroundColor = "rgb(255, 0, 200)";
+}
+
+function changeColor7() {
+    document.body.style.backgroundColor = "rgb(0, 0, 0)";
+}
+
+function changeColor8() {
+    document.body.style.backgroundColor = "rgb(255, 166, 0)";
+}
